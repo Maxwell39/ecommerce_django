d77154b02f97b01bb1e54083e899caa966a1bfb1
Kevinn Ernest <vinn.ernest.3@gmail.com>
2020-11-06 23:18:09 +0700

    ECOM-0001:database init

--- djecommerce/settings/development.py
+++ djecommerce/settings/development.py
@@ -37,16 +37,16 @@ DEBUG_TOOLBAR_CONFIG = {
 }
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    },
-    'frontend_ecom': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'frontend_ecom',
         'USER': 'blouse_01',
         'PASSWORD': 'bHybCVIN4XvKb6zF',
-        'HOST': '120.0.0.1',
+        'HOST': 'localhost',
         'PORT': '3306',
     },
 }
