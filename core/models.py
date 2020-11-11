59871551381476a6105d4757d896d5f4f6216f67
Kevinn Ernest <vinn.ernest.3@gmail.com>
2020-11-11 14:34:00 +0700

    ECOM-0001:fix

--- core/models.py
+++ core/models.py
@@ -20,6 +20,14 @@ ADDRESS_CHOICES = {
     ('S', 'Shipping')
 }
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    stripe_customer_id = models.CharField(max_length=50, blank=True, null=True)
+    one_click_purchasing = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user.username
 
 class Item(models.Model):
     title = models.CharField(max_length=100)
